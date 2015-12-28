6ef233914c233966f1331b3a73dc8e682fe06d6a
alex-B-Ly <abl12582@gmail.com>
2015-12-28 04:52:40 -0500

    revamped html to suit project

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 
-var map;
+// var map;
 
-function generateMap(){
+// function generateMap(){
 
-  var mapOptions = {
-    center: {lat: 40.4995488, lng: -74.4443186},
-    zoom: 12,
-    zoomControl: true,
-    zoomControlOptions: {
-      position: google.maps.ControlPosition.RIGHT_BOTTOM
-    }
-  }
+//   var mapOptions = {
+//     center: {lat: 40.4995488, lng: -74.4443186},
+//     zoom: 12,
+//     zoomControl: true,
+//     zoomControlOptions: {
+//       position: google.maps.ControlPosition.RIGHT_BOTTOM
+//     }
+//   }
 
-	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
+// 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+// }
 
 // FUNCTIONS CALLED
-$('.search-button').on('click', generateMap);
+// $('.search-button').on('click', generateMap);
 
 });
