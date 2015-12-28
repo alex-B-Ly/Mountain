f22021e8c2dbe5a6dfca97057af423a405fc338b
alex-B-Ly <abl12582@gmail.com>
2015-12-28 18:13:38 -0500

    Figured out logic to weather table row generation. Have to implement.

--- js/main.js
+++ js/main.js
@@ -17,7 +17,10 @@ latLon;
 				var placeName = place.data.nearest_area[0].areaName[0].value,
 				placeRegion = place.data.nearest_area[0].region[0].value,
 				lat = place.data.nearest_area[0].latitude,
-				lng = place.data.nearest_area[0].longitude;
+				lng = place.data.nearest_area[0].longitude,
+				date = new Date(),
+				weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+				today = weekdays[date.getDay()];
 
 				latLon = [];
 				latLon.push(lat);
@@ -25,9 +28,10 @@ latLon;
 
 				// Pass weather data to table in html here
 				$('.weather-table-place-name').html(placeName + ', ' + placeRegion);
+					// run a loop based on num_of_days and build weather data rows in table.
 
 				// Generate Map
-				generateMap(parseFloat(latLon[0]), parseFloat(latLon[1]));
+				// generateMap(parseFloat(latLon[0]), parseFloat(latLon[1]));
 			},
 			error: function(){
 				console.log('Weather request failed');
@@ -37,19 +41,19 @@ latLon;
 
 // 	MAP FUNCTION
 
-	function generateMap(latitude, longitude){
+	// function generateMap(latitude, longitude){
 
-	  var mapOptions = {
-	    center: {lat: latitude, lng: longitude},
-	    zoom: 12,
-	    zoomControl: true,
-	    zoomControlOptions: {
-	      position: google.maps.ControlPosition.RIGHT_BOTTOM
-	    }
-	  }
+	//   var mapOptions = {
+	//     center: {lat: latitude, lng: longitude},
+	//     zoom: 12,
+	//     zoomControl: true,
+	//     zoomControlOptions: {
+	//       position: google.maps.ControlPosition.RIGHT_BOTTOM
+	//     }
+	//   }
 
-		map = new google.maps.Map(document.getElementById('map'), mapOptions);
-	}
+	// 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	// }
 
 // MAIN FUNCTION END
 
